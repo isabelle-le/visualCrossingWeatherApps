680b51e5f7b766759aadb560457200180e72fda9
Isabelle LE <32997231+isabelle-le@users.noreply.github.com>
2022-08-18 17:12:23 +0200

    Update script_visualCrossingAPI.py

--- bin/script_visualCrossingAPI.py
+++ bin/script_visualCrossingAPI.py
@@ -1,9 +1,11 @@
 import requests as req
+import urllib
 import json
 
 
 def weather_api_call(requestURL, parameters):
     response = req.get(url=requestURL, params=parameters)
+    print(response.url)
     if response.status_code != 200:
         print(response.json())
         exit()
@@ -11,18 +13,30 @@ def weather_api_call(requestURL, parameters):
     return json.dumps(data)
 
 
-def get_single_weather(key, unitgroup="metric", contenttype='json'):
-    requestURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Paris?"
-    parameters = {"key": key, "unitGroup": unitgroup,"contentType":contenttype}
+
+def get_single_weather(key, unit_group="metric",elements="datetime,datetimeEpoch,name,address,resolvedAddress,latitude,longitude,temp,humidity,precip,solarradiation,solarenergy,stations,source", include = "obs", content_type="json"):
+    """
+    :param key: api_key QRLSXGZEPLWWUC7JLBEQAVQSE
+    :param unit_group: metric °c,km
+    :param max_distance(meters): max=200km or 200000m
+    :param elements: values
+    :param include: obs: station observation, remote: remote observation satellite/radar, fcst: forecast,Cobs: combination
+    :param content_type:json
+    :return:weather_api_call
+    """
+    requestURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + urllib.parse.quote("LFPO") +"/"+ urllib.parse.quote("today")
+    parameters = {"key": key,"unitGroup": unit_group,"elements" : elements, "include" : include,"contentType":content_type}
     return weather_api_call(requestURL, parameters)
 print('')
 print(' - Requesting weather : ')
 
+
 def main():
     key = "QRLSXGZEPLWWUC7JLBEQAVQSE"
-    upcoming_movie_list = get_single_weather(key, "metric","json")
-    data = json.loads(upcoming_movie_list)
+    get_single_weather_list = get_single_weather(key,"metric","datetime,datetimeEpoch,name,address,resolvedAddress,latitude,longitude,temp,humidity,precip,solarradiation,solarenergy,stations,source","obs,days","json")
+    data = json.loads(get_single_weather_list)
     print(json.dumps(data["days"]))
+    print(json.dumps(data["stations"]))
 
 if __name__ == "__main__":
     main()
